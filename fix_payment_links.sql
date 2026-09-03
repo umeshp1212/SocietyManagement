4e1bb02fc9a9fe91ddaa00cd0bac2c314299ec48
Umesh <umesh.patil@phicommerce.com>
2026-09-03 16:57:15 +0530

    file deleted

--- fix_payment_links.sql
+++
@@ -1,2 +0,0 @@
-UPDATE maintenance_bills SET payment_link = NULL, cashfree_order_id = NULL;
-SELECT COUNT(*) as bills_reset FROM maintenance_bills WHERE payment_link IS NULL;
--- update_maintenance_amount.sql
+++
@@ -1,4 +0,0 @@
--- Set maintenance amount for all flats (Rs 3500) and shops (Rs 5000)
-UPDATE units SET monthly_maintenance_amount = 3500 WHERE unit_type = 'FLAT';
-UPDATE units SET monthly_maintenance_amount = 5000 WHERE unit_type = 'SHOP';
-SELECT unit_type, COUNT(*) as count, monthly_maintenance_amount FROM units GROUP BY unit_type, monthly_maintenance_amount;
